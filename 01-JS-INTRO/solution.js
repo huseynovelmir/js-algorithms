988cad4e4fc9f7740ce37ace5f4fadbea804f400
Huseynov Elmir <98835917+huseynovelmir@users.noreply.github.com>
2024-08-07 16:58:54 +0400

    Js-intro is ready

---
+++ 01-JS-INTRO/solution.js
@@ -0,0 +1,95 @@
+//! task1
+// TODO: Elə kod yazın ki a dəyişənin dəyərini rəqəmlərinə ayıraraq konsola yazdırsın.Misal üçün: let a = 23; // 2 3
+
+//* Həll:
+// let a = 23;
+// console.log(parseInt(a / 10), a % 10);
+
+
+//! task2
+// Todo: Elə kod yazın ki a dəyişənin dəyərini rəqəmlərinə ayıraraq konsola yazdırsın.Misal üçün: let a = 198; // 1 8
+
+//* Həll:
+// let a = 198;
+// console.log(parseInt(a / 100), a % 10);
+
+//! task3
+// Todo: Elə kod yazın ki tələbələrin sayı və alma sayı verilmiş olsun.
+// Todo: Tələbələr arasında alamların bərabər olaraq bölündüyünü düşünün.
+// Todo: Hər kəsə neçə almanın çatdığını tapın.
+// Todo:  Misal üçün: let student = 39; let apples = 1200 // 30
+
+//* Həll:
+// let student = 39;
+// let apples = 1200;
+// console.log(apples % student);
+
+//! task4
+//Todo: Elə kod yazın ki a və b dəyişənlərinin dəyərləri verilmiş olsun.
+//Todo: Bu dəyişənlərin dəyərləri ədədlərdir.
+//Todo: a və b dəyişənlərinin dəyərlərinin cəmini tapın və iki mislini tapın.
+//Todo:  Misal üçün: let a = 3; let b = 4 // 14
+
+//* Həll:
+// let a = 3;
+// let b = 4;
+// console.log((a+b)*2);
+
+//! task5
+//Todo: Elə kod yazın ki a və b dəyişənlərinin dəyərləri verilmiş olsun.
+//Todo: Bu dəyişənlərin dəyərləri ədədlərdir.
+//Todo: a və b dəyişənlərinin dəyərlərinin hasilini tapın.
+//Todo:  Misal üçün: let a = 6; let b = 2 // 12
+
+//* Həll:
+// let a = 6;
+// let b = 2;
+// console.log(a * b);
+
+//! task6
+//Todo: Elə kod yazın ki a və b dəyişənlərinin dəyərləri verilmiş olsun.
+//Todo: Bu dəyişənlərin dəyərləri ədədlərdir.
+//Todo: b a-nı qüvvətə yüksəltsin.
+//Todo:  Misal üçün: let a = 2; let b = 3 // 8 2*2*2=8
+//Todo: Misal üçün: let a = 3; let b = 2 // 9 3*3=9
+
+//* Həll:
+// let a = 2;
+// let b = 3;
+// console.log(Math.pow(a, b));
+
+//! task7
+//Todo:  Elə kod yazın ki a dəyişənin dəyərini 10-a bölüb tam hissəsini konsola yazdırsın.
+//Todo:  Misal üçün: let a = 512; // 51
+
+//* Həll:
+// let a = 512;
+// console.log(parseInt(a / 10));
+
+//! task8
+//Todo:  Elə kod yazın ki a dəyişənin dəyərini tərsinə çevirsin və konsola yazdırsın.
+//Todo:  Misal üçün: let a = 1234; // 4321
+
+//* Həll:
+// let a = 2734;
+// let result = a.toString().split("").reverse().join("");
+// console.log(result * 1);
+
+//! task9
+//Todo:  Elə kod yazın ki üçbucağın  tərəfi və hündürlüyü verilmiş olsun.
+//Todo: üçbucağın sahəsini tapın.
+//Todo:  Misal üçün: let a = 3; let h = 4 // 6
+
+//* Həll:
+// let a = 3;
+// let h = 4;
+// console.log((a * h) / 2);
+
+//! task10
+//Todo: Elə kod yazın ki dairənin radiusu verilmiş olsun.
+//Todo: Dairənin sahəsini tapın.
+//Todo:  Misal üçün: let r = 3; // 28.26
+
+//* Həll:
+// let r = 3;
+// console.log(Math.PI * Math.pow(r, 2));
---
+++ 01-JS-INTRO/task.js
@@ -0,0 +1,77 @@
+//! task1
+// TODO: Elə kod yazın ki a dəyişənin dəyərini rəqəmlərinə ayıraraq konsola yazdırsın.Misal üçün: let a = 23; // 2 3
+
+//* Həll:
+
+
+//! task2
+// Todo: Elə kod yazın ki a dəyişənin dəyərini rəqəmlərinə ayıraraq konsola yazdırsın.Misal üçün: let a = 198; // 1 8
+
+//* Həll:
+
+
+//! task3
+// Todo: Elə kod yazın ki tələbələrin sayı və alma sayı verilmiş olsun.
+// Todo: Tələbələr arasında alamların bərabər olaraq bölündüyünü düşünün.
+// Todo: Hər kəsə neçə almanın çatdığını tapın.
+// Todo:  Misal üçün: let student = 39; let apples = 1200 // 30
+
+//* Həll:
+
+
+//! task4
+//Todo: Elə kod yazın ki a və b dəyişənlərinin dəyərləri verilmiş olsun.
+//Todo: Bu dəyişənlərin dəyərləri ədədlərdir.
+//Todo: a və b dəyişənlərinin dəyərlərinin cəmini tapın və iki mislini tapın.
+//Todo:  Misal üçün: let a = 3; let b = 4 // 14
+
+//* Həll:
+
+
+//! task5
+//Todo: Elə kod yazın ki a və b dəyişənlərinin dəyərləri verilmiş olsun.
+//Todo: Bu dəyişənlərin dəyərləri ədədlərdir.
+//Todo: a və b dəyişənlərinin dəyərlərinin hasilini tapın.
+//Todo:  Misal üçün: let a = 6; let b = 2 // 12
+
+//* Həll:
+
+
+//! task6
+//Todo: Elə kod yazın ki a və b dəyişənlərinin dəyərləri verilmiş olsun.
+//Todo: Bu dəyişənlərin dəyərləri ədədlərdir.
+//Todo: b a-nı qüvvətə yüksəltsin.
+//Todo:  Misal üçün: let a = 2; let b = 3 // 8 2*2*2=8
+//Todo: Misal üçün: let a = 3; let b = 2 // 9 3*3=9
+
+//* Həll:
+
+
+//! task7
+//Todo:  Elə kod yazın ki a dəyişənin dəyərini 10-a bölüb tam hissəsini konsola yazdırsın.
+//Todo:  Misal üçün: let a = 512; // 51
+
+//* Həll:
+
+
+//! task8
+//Todo:  Elə kod yazın ki a dəyişənin dəyərini tərsinə çevirsin və konsola yazdırsın.
+//Todo:  Misal üçün: let a = 1234; // 4321
+
+//* Həll:
+
+
+//! task9
+//Todo:  Elə kod yazın ki üçbucağın  tərəfi və hündürlüyü verilmiş olsun.
+//Todo: üçbucağın sahəsini tapın.
+//Todo:  Misal üçün: let a = 3; let h = 4 // 6
+
+//* Həll:
+
+
+//! task10
+//Todo: Elə kod yazın ki dairənin radiusu verilmiş olsun.
+//Todo: Dairənin sahəsini tapın.
+//Todo:  Misal üçün: let r = 3; // 28.26
+
+//* Həll:
